53b8bb78a8e641b4bbc9cd9504f4bf54de332deb
Andrey Begeka <dushessssssss45@gmail.com>
2024-11-06 22:44:37 +0300

    fixed rights for files in dockerfile config

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MainController extends Controller
+{
+    public function test()
+    {
+        $test = 'test';
+        return view('main_page', compact('test'));
+
+    }
+}
